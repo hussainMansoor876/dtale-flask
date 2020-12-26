9de294ed1225a49d619eef96e08c66457f30d2ed
mansoor876 <69988975+mansoor876@users.noreply.github.com>
2020-12-26 13:36:55 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,3 @@
+from dtale import app
+
+app.show(name="Hello", host='localhost', port=5001, debug=True)
---
+++ test_env.js
@@ -0,0 +1,15 @@
+import "regenerator-runtime/runtime";
+
+import { configure } from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+
+configure({ adapter: new Adapter() });
+
+// required for react-data-grid & react-bootstrap-modal in react 16
+require("./static/adapter-for-react-16");
+
+// this file is compiled in an odd way so we need to mock it (react-syntax-highlighter)
+jest.mock("react-syntax-highlighter/dist/esm/styles/hljs", () => ({ docco: {} }));
+
+// this is required for webpack dynamic public path setup
+global.__webpack_public_path__ = "";
